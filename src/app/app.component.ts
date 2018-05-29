e906d2405d7a248bce07d4c5424164bcfbd724a9
Adam Young <ajyoung@atomicsoftware.com>
2018-05-28 21:51:48 -0400

    converts to keyword search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,32 +8,33 @@ import 'rxjs/add/operator/map';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private metroBoominURL = 'http://musicbrainz.org/ws/2/artist/59db3d82-86ea-451f-881f-dffc8ec387c9?inc=aliases&fmt=json';
+  private musicBrainzSearchURL = 'http://musicbrainz.org/ws/2/artist/?query=artist:';
+  private myMusicBrainzSearchURL = '';
 
   title = 'TrackBase';
-  overviewData: any = {};
-  musicbrainzID = '59db3d82-86ea-451f-881f-dffc8ec387c9';
+  searchTerm = 'metroboomin';
+  searchData: any = {};
 
   constructor(private http: Http) {
     console.log("AppComponent constructor");
-    this.getOverviewData();
-    this.getArtistDetails();
+    this.getSearchData();
+    this.executeMusicBrainzSearch();
   }
 
-  getOverviewData() {
-    console.log("AppComponent getOverviewData");
-    this.metroBoominURL = 'http://musicbrainz.org/ws/2/artist/'
-      + this.musicbrainzID
-      + '?inc=aliases&fmt=json';
-    return this.http.get(this.metroBoominURL)
+  getSearchData() {
+    console.log("AppComponent getSearchData");
+    this.myMusicBrainzSearchURL = this.musicBrainzSearchURL 
+      + this.searchTerm
+      + '&fmt=json';
+    return this.http.get(this.myMusicBrainzSearchURL)
       .map((res: Response) => res.json());
   }
 
-  getArtistDetails() {
-    this.getOverviewData().subscribe(data => {
-        console.log("AppComponent.getArtistDetails: " + data);
+  executeMusicBrainzSearch() {
+    this.getSearchData().subscribe(data => {
+        console.log("AppComponent.executeMusicBrainzSearch: " + data);
         console.log(data);
-        this.overviewData = data;
+        this.searchData = data;
     })
   }
 }
